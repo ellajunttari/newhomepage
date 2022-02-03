b60edcc1e6b011c6525b10198acd78d6ee4424db
Ella Junttari <ellamarjut@gmail.com>
2022-02-03 13:21:19 +0200

    added the currect versions of all the files

---
+++ home.js
@@ -0,0 +1,19 @@
+window.onload = function() {
+
+  
+}
+
+function login() {
+  var loginDiv = document.getElementById("modaali");
+  loginDiv.style.display ="block";
+}
+
+function spanClick() {
+  var modaali = document.getElementById("modaali");
+  modaali.style.display='none';
+}
+
+function cancelButton() {
+  var modaali = document.getElementById('modaali');
+  modaali.style.display='none';
+}
